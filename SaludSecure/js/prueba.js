3c13263db58c3be21e9134a3923ae003876db01c
arifai1 <101335409+arifai1@users.noreply.github.com>
2022-09-06 15:00:00 -0300

    Revert "tried to unify front and back. Not working"
    This reverts commit c3f79a8f8508dee3ec13e015949f68b3acd4644c.

--- SaludSecure/js/prueba.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){                                                                                               
-
-$("#LogIn").click(function(){
-    
-    window.location.replace('../html/pantallaprincipal.html');
-    
-    });
-})
