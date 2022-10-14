e2a937bd78278edc6528d0673dbc9738400fd162
arifai1 <101335409+arifai1@users.noreply.github.com>
2022-10-14 10:49:29 -0300

    SESSIONS completed
    Did sessions, now you cant enter pantallaprincipal.php and pantallaprincipal_doc.php before sign in or register. Can do it with html files

---
+++ SaludSecure/php/recetas.php
@@ -0,0 +1,31 @@
+<?php session_start();
+if (!isset($_SESSION['user'])){
+    header('Location: ../html/pantallainicio.html');
+    }?>
+
+<!DOCTYPE html>
+
+<head>
+	<meta http-equiv="Contet-Type" content="text/html; charset=UTF-8">
+	<title>Mis Recetas</title>
+
+	<script src="../js/jquery-3.6.0.min.js" type="text/javascript"></script>
+	<script src="../js/saludsecure.js" type="text/javascript"></script>
+
+</head>
+
+<body>
+	<div ud="contenedor">
+		<div id="log">
+			<label id="5" class="caja">Busca tu receta!</label>
+			<form id="form" role="search">
+				<input type="search" id="query" name="q" placeholder="Search..."
+					aria-label="Search through site content">
+				<input type="submit" value="Buscar" id="Buscar" class="boton" />
+			</form>
+			<a href="./pantallaprincipal.php">Regresar</a>
+
+			<div id="divt">
+			
+			
+</body>
