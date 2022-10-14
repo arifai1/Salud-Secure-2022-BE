527057ea1cf291e69bb81ce6cb3d4400d860c4a4
arifai1 <101335409+arifai1@users.noreply.github.com>
2022-10-14 10:47:52 -0300

    SESSIONS completed.
    Now you cant go to Pantallapricipal or Pantallaprincipal_doc and any of their buttons before sign in or register. Can do it with html but not with php

--- SaludSecure/php/pantallaprincipal_doc.php
+++ SaludSecure/php/pantallaprincipal_doc.php
@@ -1,4 +1,7 @@
-<?php session_start();?> 
+<?php session_start();
+if (!isset($_SESSION['user'])){
+    header('Location: ../html/pantallainicio.html');
+    }?> 
 
 <!DOCTYPE html>
 <html lang="en">
