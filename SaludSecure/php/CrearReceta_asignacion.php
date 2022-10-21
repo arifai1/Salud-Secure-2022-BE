4cd7c757886c7a9d22a9bfb9c68fbf0d1c8d26db
arifai1 <101335409+arifai1@users.noreply.github.com>
2022-10-21 11:53:11 -0300

    Update CrearReceta_asignacion.php

--- SaludSecure/php/CrearReceta_asignacion.php
+++ SaludSecure/php/CrearReceta_asignacion.php
@@ -5,7 +5,7 @@
 
     $nombrepac= $_POST["usuario"];
     $id = "SELECT idpaciente FROM `paciente` WHERE usuario = $nombrepac";
-    $sql = "INSERT INTO medico_paciente (idmedico,idpaciente) VALUES (" . $_SESSION['user'] . "," . $id . ")";     //Falta hacer SESSIONS para que el ID del medico sea obligatoriamente con el que se Loguea, como una firma. De esta manera vamos a poder hacer luego la autoasignacion con 2 SELECT más. 
+    $sql = "INSERT INTO medico_paciente (idmedico,idpaciente) VALUES (" . $_SESSION['user'] . "," . $id . ")"; 
 	
     echo json_encode($data);
     $con->close();	
