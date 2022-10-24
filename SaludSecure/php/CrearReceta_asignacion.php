9a296d83a06fdead4f40211583726c1a44ff78de
arifai1 <101335409+arifai1@users.noreply.github.com>
2022-10-24 14:32:30 -0300

    Update CrearReceta_asignacion.php

--- SaludSecure/php/CrearReceta_asignacion.php
+++ SaludSecure/php/CrearReceta_asignacion.php
@@ -5,7 +5,7 @@
     $id = "SELECT idpaciente FROM `paciente` WHERE usuario = $nombrepac";
     $sql = "INSERT INTO medico_paciente (idmedico,idpaciente) VALUES (" . $_SESSION['user'] . "," . $id . ")"; 
 	
-    //falta hacer la query
+    //falta hacer la query para poder modificar la bbdd y hacer la asignacion. 
 
     echo json_encode($data);
     $con->close();	
