a53dfcdf30c67aecca166e37ee9f1e3c0e125651
arifai1 <101335409+arifai1@users.noreply.github.com>
2022-10-31 14:51:52 -0300

    updated, now you can do the SELECT DISTINCT so that you can get the information of the PACIENTE without being repeated many times

--- SaludSecure/php/CrearReceta_asignacion.php
+++ SaludSecure/php/CrearReceta_asignacion.php
@@ -16,16 +16,13 @@
     // var_dump($res2);
     if($res2){														
         $data['status']='ok';   //falta agarrar los datos y mostrarlos en las pantallas correspondientes.
-        $sql2 = "SELECT nombre, apellido, credencial, nacimiento, usuario FROM paciente LEFT JOIN medico_paciente ON (paciente.idpaciente = medico_paciente.idpaciente) WHERE medico_paciente.idmedico = '". $_SESSION["user"] ."'"; //GROUP BY idpaciente;
+        $sql2 = "SELECT DISTINCT nombre, apellido, credencial, nacimiento, usuario FROM paciente LEFT JOIN medico_paciente ON (paciente.idpaciente = medico_paciente.idpaciente) WHERE medico_paciente.idmedico = '". $_SESSION["user"] ."'"; //hacemos un SELECT DISTINCT en vez de un GROUP BY para que me seleccione una sola vez los pacientes, sin repetirlos. 
         $res3 = $con->query($sql2);
-
         if($res3->num_rows > 0){
-            $userData3 = array();
-            
+            $userData3 = array();            
             while($fila = $res3->fetch_assoc()){
                 $userData3[] = $fila;
             }
-
             $data['status']='ok';
             $data['result']= $userData3;
         }else{
