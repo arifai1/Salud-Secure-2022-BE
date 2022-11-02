bb17f09fc3f9a59e95953bd323fd110cea811496
VELO BARES ABRIL <47206356@ort>
2022-11-02 10:25:50 -0300

    lol

--- SaludSecure/php/CrearReceta.php
+++ SaludSecure/php/CrearReceta.php
@@ -11,6 +11,7 @@ if (!isset($_SESSION['user'])){
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <UTF-8>
         <script src="../js/jquery-3.6.0.min.js" type="text/javascript"></script>
+        <script src="../js/metaMask.js" type="text/javascript"></script>
         <script src="../js/saludsecure.js" type="text/javascript"></script>
         <script src="../js/SobreBesmo.js" type="text/javascript"></script>
 	    <link rel="Icon" href="../imagenes/logo-Header.png">
