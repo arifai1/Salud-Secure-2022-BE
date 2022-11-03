4b67849e449201cdfeff60518280104eaad0b43d
CaroKaufman <101335676+CaroKaufman@users.noreply.github.com>
2022-11-03 13:20:18 -0300

    arreglo btn enviar

--- SaludSecure/js/SobreBesmo.js
+++ SaludSecure/js/SobreBesmo.js
@@ -6,42 +6,6 @@ $(document).ready(function() {
  
  
  
-<<<<<<< Updated upstream
- $(".receta").css({transition: ".2s all ease"})
-  let izq = 300;
-  let der = 0;
-  $("#Izq").click(()=>{
-    izq -= 250;
-    $(".receta").css({"position": "relative", top: "14%", left: izq-der})
-  
-  })
-  $("#Der").click(()=>{
-    der -= 250;
-    $(".receta").css({"position": "relative", top: "14%", left: izq-der})
-  })
-   // $(function() {
-        $( "#EnviarSC" ).click(function() {
-           // alert ("hola");
-          $( "#EnviarSC" ).addClass( "onclic", 250, validate());
-        });
-      
-        function validate() {
-          setTimeout(function() {
-            $( "#EnviarSC" ).removeClass( "onclic" );
-            $( "#EnviarSC" ).addClass( "validate", 450, callback());
-          }, 2250 );
-        }
-          function callback() {
-            setTimeout(function() {
-              $( "#EnviarSC" ).removeClass( "validate" );
-            }, 1250 );
-          }
-
-
-
-          $( "#receta" ).click(function() {
-          
-=======
   $(".receta").css({transition: ".2s all ease"})
    let izq = 300;
    let der = 0;
@@ -56,7 +20,6 @@ $(document).ready(function() {
    })
     // $(function() {
          $( "#button" ).click(function() {
->>>>>>> Stashed changes
             // alert ("hola");
            $( "#button" ).addClass( "onclic", 250, validate());
          });
