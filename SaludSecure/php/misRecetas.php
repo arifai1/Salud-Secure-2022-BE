20ee1c35215ed10b03212c5f1b7ef148235886bd
AbrilVelo <101335470+AbrilVelo@users.noreply.github.com>
2022-11-03 14:08:49 -0300

    ANDA BOTON:))

--- SaludSecure/php/misRecetas.php
+++ SaludSecure/php/misRecetas.php
@@ -12,6 +12,7 @@ if (!isset($_SESSION['user'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="../js/jquery-3.6.0.min.js" type="text/javascript"></script>
+		
         <script src="../js/saludsecure.js" type="text/javascript"></script>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <script src="../js/SobreBesmo.js" type="text/javascript"></script>
@@ -19,45 +20,17 @@ if (!isset($_SESSION['user'])){
 	<link rel="Icon" href="../imagenes/logo-Header.png">
     
    
-    <title>Document</title>
-</head>
-<body>
-    <header>
-        <label id="Txtlogo">BESMO</label>
-        
-        <label id="headertitle"> Mis Recetas </label>
-        <input id="logo"type="button">
-        <div id="LogOut">
-            <input type ="button" class="minibutton" value="Log Out"/>
-        </div>
-        <input id="Usuario"type ="button"/>
-    </header>
-    <div id="recetasContainer">
-        
-        <div id="item1"class="receta">
-        <label id="lbl1">Tratamiento</label>
-        <label id="lbl2">Fecha</label>
-        <input type="submit" value="Ver" id="verReceta" class="minibutton" onclick="myFunction()"/> 
-  
-        
-        </div>
-       
-       
-    </div>
-    
-    <a id="Izq"><i id="chevron_left" class="material-icons">chevron_left</i></a>
-    <a id="Der"><i id="chevron_right" class="material-icons">chevron_right
-    </i></a>
-    
-    <input type="button" value="?" class="ayuda">
-    <a class="btn-floating btn-large waves-effect" id="RegresarP"><i id="IconregresarP" class="material-icons">arrow_back</i></a>
-   
-    <script>
+    <title>Mis Recetas</title>
+	
+	</head>
+
+	
+	<script>
         //CODIGO PARA LEER RECETA --> PACIENTE
         
-        import Web3 from 'web3';
+        const Web3 = new Web3(window.ethereum);
 
-        //const web3 = new Web3(window.ethereum);
+        //const web3 = 
         //await window.ethereum.enable();
 
        const contract_abi = [
@@ -119,7 +92,7 @@ if (!isset($_SESSION['user'])){
 		"type": "function"
 	}
 ]
-//$(document).ready(function() {
+
 	async function myFunction() {
   			alert("holapa");
             const contract_address = '0xa0ccDD96AE52777f1eCe7D1efF6A02ae7341614b' ;
@@ -129,24 +102,42 @@ if (!isset($_SESSION['user'])){
                     alert(result) 
              })
     }
+	const provider = new ethers.providers.JsonRpcProvider('https://rpc-mumbai.matic.today'); 
+       </script>
+	<script type="module" src="https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js"></script>
+<body>
 
-// $(document).ready(function() {
-//         $('#verReceta').click(function(){
-//             console.log("funca");
-//             alert("viendo");
-//             const contract_address = '0xa0ccDD96AE52777f1eCe7D1efF6A02ae7341614b' ;
-//             const SaludSecure = new ethers.Contract(contract_address,contract_abi, provider); 
-//             const txn = SaludSecure.methods.ver_Receta().call(); 
-//              txn.then(function(result) {
-//                     alert(result) 
-//              })
-//         });
-//     });
-        const provider = new ethers.providers.JsonRpcProvider('https://rpc-mumbai.matic.today'); 
+    <header>
+        <label id="Txtlogo">BESMO</label>
         
-//}
-          //FIJARSE SI ESTA BIEN LOS NOMBRES
-        </script>
+        <label id="headertitle"> Mis Recetas </label>
+        <input id="logo"type="button">
+        <div id="LogOut">
+            <input type ="button" class="minibutton" value="Log Out"/>
+        </div>
+        <input id="Usuario"type ="button"/>
+    </header>
+    
+	<div id="recetasContainer">
+        
+        <div id="item1"class="receta">
+        <label id="lbl1">Tratamiento</label>
+        <label id="lbl2">Fecha</label>
+        <input type="submit" value="Ver" id="verReceta" class="minibutton" onclick="myFunction()"/> 
+  
+        
+        </div>
+       
+    </div>
+    
+    <a id="Izq"><i id="chevron_left" class="material-icons">chevron_left</i></a>
+    <a id="Der"><i id="chevron_right" class="material-icons">chevron_right
+    </i></a>
+    
+    <input type="button" value="?" class="ayuda">
+    <a class="btn-floating btn-large waves-effect" id="RegresarP"><i id="IconregresarP" class="material-icons">arrow_back</i></a>
+   
+    
         
  
 </body>
