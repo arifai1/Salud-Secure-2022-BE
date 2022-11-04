74fefc97134d9179a93d52cb73261192ce88bb9a
AbrilVelo <101335470+AbrilVelo@users.noreply.github.com>
2022-11-04 07:59:27 -0300

    cambio caro

--- SaludSecure/js/SobreBesmo.js
+++ SaludSecure/js/SobreBesmo.js
@@ -20,16 +20,55 @@ $(document).ready(function() {
    })
 
 
+
     // $(function() {
-<<<<<<< Updated upstream
+
+
+
+
+
+
+
+   async function conexionWeb3(){
+        
+    //import detectEthereumProvider from '@metamask/detect-provider';
+     if (typeof window.ethereum !== "undefined") {
+               ethereum.request({ method: "eth_requestAccounts" })
+              //connectWallet()
+                 //sendReceta();
+                 await window.ethereum.enable()
+                 
+                 
+             //     mensajeM = "Conectando con MetaMask";                                               //Href me indica destino.
+             //   $("#divt").html(mensajeM);
+             //   $("#divt").show();
+              }
+    else{
+        alert("No tiene MetaMask instalado, por favor descarguelo");
+               mensaje = "No tiene instalado MetaMask, por favor instalelo apretando el boton 'Conectar con MetaMask'";                                               //Href me indica destino.
+               $("#divt").html(mensaje);
+               $("#divt").show();
+               await window.open("https://metamask.io/download/", "_blank")
+     }
+       }
+
+        async function connectWallet() {
+               const provider = new ethers.providers.JsonRpcProvider('https://rpc-mumbai.matic.today');
+               await provider.send("eth_requestAccounts", []);
+               const signer = provider.getSigner()
+               alert("Conectado");
+        }
+   
+
          $( "#EnviarSC" ).click(function() {
-            // alert ("hola");
+            // envia a la funcion de giro
            $( "#EnviarSC" ).addClass( "onclic", 250, validate());
-=======
+
+
          $( "#button" ).click(function() {
             // Agrega la clase de giro y llama la funcion validar
            $( "#button" ).addClass( "onclic", 250, validate());
->>>>>>> Stashed changes
+
          });
        
          function validate() {
@@ -48,8 +87,9 @@ $(document).ready(function() {
  
            $( "#receta" ).click(function() {
            
-             // alert ("hola");
+           
              $( "#receta" ).addClass( "recetaIzq");
           });
-     //    });
- });
+     
+ });   
+});
