ed0e184b98f18eb635f089238309657cd63b7d18
AbrilVelo <101335470+AbrilVelo@users.noreply.github.com>
2022-11-04 07:59:46 -0300

    caro ort

--- SaludSecure/php/CrearReceta.php
+++ SaludSecure/php/CrearReceta.php
@@ -76,32 +76,7 @@ if (!isset($_SESSION['user'])){
     
         
     
-    $(function() {
-        $( "#button" ).click(function() {
-           // alert ("hola");
-          $( "#button" ).addClass( "onclic", 250, validate());
-        });
-      
-        function validate() {
-          setTimeout(function() {
-            $( "#button" ).removeClass( "onclic" );
-            $( "#button" ).addClass( "validate", 450, callback());
-          }, 2250 );
-        }
-          function callback() {
-            setTimeout(function() {
-              $( "#button" ).removeClass( "validate" );
-            }, 1250 );
-          }
-
-
-
-          $( "#receta" ).click(function() {
-          
-            // alert ("hola");
-            $( "#receta" ).addClass( "recetaIzq");
-         });
-        });
+    
     </script>
 <body>
 
