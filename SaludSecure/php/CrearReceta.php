5038d0020bac7210f979adde81d045bfd62ba952
arifai1 <101335409+arifai1@users.noreply.github.com>
2022-11-15 15:42:44 -0300

    updated. It is working as i used another link
    link used:
    http://localhost/Salud-Secure-2022-BE/SaludSecure/html/pantallainicio.html

--- SaludSecure/php/CrearReceta.php
+++ SaludSecure/php/CrearReceta.php
@@ -13,6 +13,7 @@ if (!isset($_SESSION['user'])){
         <script src="../js/jquery-3.6.0.min.js" type="text/javascript"></script>
         <script src="../js/saludSecureABI.js" type="text/javascript"></script>
         <script src="../js/asignacionPyM.js" type="text/javascript"></script>
+        <script src="../js/saludsecure.js" type="text/javascript"></script>
         <script src="../js/SobreBesmo.js" type="text/javascript"></script>
 	    <link rel="Icon" href="../imagenes/logo-Header.png">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
