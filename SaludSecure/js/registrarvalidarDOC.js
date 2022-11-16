7793bae2817711e4fade97070a5ebbf79885d9a9
ValentinaHsu <92767924+ValentinaHsu@users.noreply.github.com>
2022-11-16 13:39:49 -0300

    Docs Update

--- SaludSecure/js/registrarvalidarDOC.js
+++ SaludSecure/js/registrarvalidarDOC.js
@@ -87,42 +87,6 @@ $(document).ready(function () {
     $("#RegistrarMed").click(function () {
         $("#RegistrarMed").removeClass("minibutton");
         $("#RegistrarMed").addClass("minibuttonClick");
-        if ($("#UsuMed").val() == '' || $("#PassMed").val() == '' || $("#NomMed").val() == '' || $("#ApeMed").val() == '' || $("#AreaMed").val() == '' || $("#teleMed").val() == '') {
-            $("#divAgregardatosM").html("Hay campos vacios");
-            $("#divAgregardatosM").show();
-            $("#u").removeClass("ingresar");
-            $("#u").addClass("noIngresado");
-            $("#p").removeClass("ingresar");
-            $("#p").addClass("noIngresado");
-        } else {
-            $.ajax({
-                type: 'POST',
-                url: '../php/registrar_doc.php',
-                dataType: "json",
-                data: 'usum=' + $("#UsuMed").val() + '&passm=' + $("#PassMed").val() + '&nom=' + $("#NomMed").val() + '&ape=' + $("#ApeMed").val() + '&Aream=' + $("#AreaMed").val() + '&telefonoMed=' + $("#teleMed").val(), //'&DNI='+$("#DNIMed").val()+
-                success: function (data) {
-                    if (data.status == 'ok') {
-                        mensaje = "Se registró correctamente el usuario";
-                        $("#msgDoc").html(mensaje);
-                        $("#msgDoc").show();
-                        window.location.replace('../php/pantallaprincipal_doc.php');
-                    }
-                    else if (data.status == 'err') {
-                        mensaje = "El usuario que intento ingresar ya existe";      //lo tira en cualquier lado
-                        $("#msgDoc").html(mensaje);
-                        $("#msgDoc").show();
-                    }
-                    else {
-                        mensaje = "Ocurrio un error";
-                        $("#msgDoc").html(mensaje);
-                        $("#msgDoc").show();
-                    }
-                },
-                error: function (error) {
-                    ;
-                },
-            });
-        }
     });
 
     $("#RegistrarMed").mouseover(function () {
